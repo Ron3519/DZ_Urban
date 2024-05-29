fc86a927719a5907c5432d268384f23d6e573792
Ron3519 <166723052+Ron3519@users.noreply.github.com>
2024-05-29 15:46:54 +0500

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Aleksey':1991,'Ron':2008,'End':2010}
+print(my_dict)
+print(my_dict['Ron'],my_dict.get('Fog'))
+my_dict['Biba'] = 2014
+my_dict['Gorillaz'] = 1998
+print(my_dict.pop('End'))
+print(my_dict)
+my_set = {1 , 1 ,2 ,444 ,2 ,4 }
+print(my_set)
+my_set.add(6)
+my_set.add(0)
+
+my_set.remove(444)
+print(my_set)
