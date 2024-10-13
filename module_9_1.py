893d67a287a0df673ebd13e572beb31a6803b8c5
Ron3519 <166723052+Ron3519@users.noreply.github.com>
2024-10-13 13:02:26 +0500

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,13 @@
+def apply_all_func(int_list, *functions):
+    result = {}
+    for i in functions:
+        result[i.__name__] = map(i, int_list)
+
+    return result
+
+
+
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
